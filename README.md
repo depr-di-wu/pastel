007303035879990781bb1734010805c1de5ae673
Quint <me@di-wu.be>
2020-12-22 10:08:08 +0100

    Start with blank screen.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Pastel
 A fully custom Emacs configuration.
 
+## How to run?
+```shell
+emacs -q -l init.el
+```
--- init.el
+++ init.el
@@ -1,2 +1,5 @@
-;; Empty for the time being...
-
+;; Initial (blank) screen.
+(setq inhibit-startup-message t) ; Remove startup message.
+(scroll-bar-mode -1)             ; Remove toolbars.
+(tool-bar-mode -1)
+(menu-bar-mode -1)
